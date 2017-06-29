8aa2da47dbc4d2f92a4f293786633cd9d532e831
Socrates Hlapolosa <socrates.hlapolosa@gmail.com>
2017-06-29 20:30:45 +0200

    writing combined yaml to disc

--- server.js
+++ server.js
@@ -13,31 +13,39 @@ var middleware = require('swagger-express-middleware');
 var path = require('path');
 var app = express();
 var swaggerCombine = require('swagger-combine');
+var yaml = require('write-yaml');
 
 app.get('/swagger.yaml', swaggerCombine.middleware('docs/swagger.yaml', {format: 'yaml'}));
 
-swaggerCombine('docs/swagger.yaml').then(combined => passIntoMiddleWare(combined)).catch(error => console.log('an error occured: '+error));
+swaggerCombine('docs/swagger.yaml').then(combined =>
+passIntoMiddleWare(combined)
+).
+catch(error => console.log('an error occured: ' + error)
+)
+;
 
 function passIntoMiddleWare(combined) {
 
-  middleware(combined, app, function (err, middleware) {
+    yaml.sync('combined.yaml', combined);
 
-    app.use(
-      middleware.metadata(),
-      middleware.CORS(),
-      middleware.files(),
-      middleware.parseRequest(),
-      middleware.validateRequest(),
-      middleware.mock()
-    );
+    middleware(combined, app, function (err, middleware) {
 
-    app.listen(8000, function () {
-      console.log('=============================================================');
-      console.log('Stubs running on  http://localhost:8000');
-      console.log('To view combined contract  http://localhost:8000/swagger.yaml');
-      console.log('=============================================================');
+        app.use(
+            middleware.metadata(),
+            middleware.CORS(),
+            middleware.files(),
+            middleware.parseRequest(),
+            middleware.validateRequest(),
+            middleware.mock()
+        );
+
+        app.listen(8000, function () {
+            console.log('=============================================================');
+            console.log('Stubs running on  http://localhost:8000');
+            console.log('To view combined contract  http://localhost:8000/swagger.yaml');
+            console.log('=============================================================');
+        });
     });
-  });
 };
 
 
