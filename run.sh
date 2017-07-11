e592ee249e4e4e94956a2bf0bf7adf949838f1b5
Maciej Matys <maciej.matys@intive.com>
2017-07-11 12:21:28 +0200

    Fix problem with missing Header.yaml file

--- run.sh
+++ run.sh
@@ -2,5 +2,10 @@
 cd merge-yml
 mvn -P uberjar package
 cd ../
-./merge-yml/bin/merge-yml.sh api/swagger/contracts/Headers.yaml api/swagger/contracts/NedbankID.yaml api/swagger/contracts/FicaMe.yaml api/swagger/contracts/SCPExpenses.yaml > api/swagger/swagger.yaml
+./merge-yml/bin/merge-yml.sh \
+  api/swagger/contracts/Test.yaml \
+  api/swagger/contracts/NedbankID.yaml \
+  api/swagger/contracts/FicaMe.yaml \
+  api/swagger/contracts/SCPExpenses.yaml \
+  > api/swagger/swagger.yaml
 swagger project edit -p 10011 &
