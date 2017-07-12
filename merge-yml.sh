5bc9fe1e0755a053b23c9bf3ff64c56fe7bac0ba
Maciej Matys <maciej.matys@intive.com>
2017-07-12 14:18:09 +0200

    Extract Header.yaml

--- merge-yml.sh
+++ merge-yml.sh
@@ -4,4 +4,5 @@
   api/swagger/contracts/NedbankID.yaml \
   api/swagger/contracts/FicaMe.yaml \
   api/swagger/contracts/SCPExpenses.yaml \
+  api/swagger/contracts/Header.yaml \
 > api/swagger/swagger.yaml
