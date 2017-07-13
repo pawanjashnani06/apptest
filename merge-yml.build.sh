e7755f251ac6f6f884c72626fb36683cadea04e4
Maciej Matys <maciej.matys@intive.com>
2017-07-13 15:52:22 +0200

    Use maven docker image to build merge-yml

--- merge-yml.build.sh
+++ merge-yml.build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-mvn -f merge-yml/pom.xml -P uberjar package
+docker run -it --rm \
+    --name merge-yml.build \
+    -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:alpine \
+    mvn -f merge-yml/pom.xml -P uberjar package
