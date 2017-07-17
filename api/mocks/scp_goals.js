44301050d073be487cc1c73a9fb2054e8a0365ed
Shivam-Oberoi <shivam.oberoi@nagarro.com>
2017-07-18 00:41:44 +0530

    Upadting Goals Api for Post. (#28)

--- api/mocks/scp_goals.js
+++ api/mocks/scp_goals.js
@@ -2,7 +2,8 @@
 
 module.exports = {
   goals: goals,
-  goaltypes: goaltypes
+  goaltypes: goaltypes,
+  goalsForPost: goalsForPost
 }
 
 function goals (req, res, next) {
@@ -31,6 +32,23 @@ function goaltypes (req, res, next) {
         'GoalIconClass': 'Sample text',
         'GoalCategoryImageTag': 'Sample text',
         'GoalDefaultImageTag': 'Sample text'
+      }
+    ]
+  ])
+}
+
+function goalsForPost (req, res, next) {
+  res.json([
+    [
+      {
+        'GoalIdentifier': '',
+        'GoalTypeId': '1',
+        'TargetAmount': 5000,
+        'TargetDate': '2016-08-10',
+        'ContributionAmount': 0,
+        'GoalAccount': '1009427709',
+        'GoalName': 'Laptop',
+        'GoalPicture': '/content/dam/nedbank-scp/site-assets/goals/laptop.jpg'
       }
     ]
   ])
