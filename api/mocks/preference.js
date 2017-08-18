3c9963f85a1f8acc56dbb32c130ed9332d69b518
pawanjashnani06 <pawan.jashnani@nagarro.com>
2017-08-18 12:39:12 +0530

    Updated response for limit API. (#67)
    * Updated reponse for limit API.
    
    * Updated latest reponse for limit API.

--- api/mocks/preference.js
+++ api/mocks/preference.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   getLimits: getLimits,
+  getLimitsLatest: getLimitsLatest,
   getSortOrder: getSortOrder
 }
 
@@ -112,6 +113,76 @@ function getLimits (req, res, next) {
           ]
         }
       ]
+    }
+  })
+}
+
+function getLimitsLatest (req, res, next) {
+  res.json({
+    'data': [
+      {
+        'limitType': 'transfer',
+        'dailyLimit': 150000,
+        'userAvailableDailyLimit': 150000,
+        'maxDailyLimit': 150000,
+        'isTempLimit': false,
+        'maxTmpDateRangeLimit': 30
+      },
+      {
+        'limitType': 'payment',
+        'dailyLimit': 150000,
+        'userAvailableDailyLimit': 150000,
+        'maxDailyLimit': 150000,
+        'isTempLimit': false,
+        'maxTmpDateRangeLimit': 30
+      },
+      {
+        'limitType': 'lotto',
+        'dailyLimit': 599,
+        'userAvailableDailyLimit': 599,
+        'maxDailyLimit': 1000,
+        'isTempLimit': false,
+        'maxTmpDateRangeLimit': 30
+      },
+      {
+        'limitType': 'sendimali',
+        'dailyLimit': 2500,
+        'userAvailableDailyLimit': 2500,
+        'maxDailyLimit': 2500,
+        'isTempLimit': false,
+        'maxTmpDateRangeLimit': 30
+      },
+      {
+        'limitType': 'prepaid',
+        'dailyLimit': 3000,
+        'userAvailableDailyLimit': 3000,
+        'maxDailyLimit': 3000,
+        'isTempLimit': false,
+        'maxTmpDateRangeLimit': 30
+      },
+      {
+        'limitType': 'instantpayment',
+        'dailyLimit': 9600,
+        'userAvailableDailyLimit': 9600,
+        'maxDailyLimit': 10000,
+        'isTempLimit': true,
+        'tempLimitStart': '2017-08-17T00:00:00',
+        'tempLimitEnd': '2017-08-22T00:00:00',
+        'tempDailyLimit': 9600,
+        'tempAvailableDailyLimit': 9600,
+        'lastPermanentLimit': 1000,
+        'maxTmpDateRangeLimit': 30
+      },
+      {
+        'limitType': 'visapayment',
+        'dailyLimit': 80000,
+        'userAvailableDailyLimit': 80000,
+        'maxMonthlyLimit': 80000,
+        'isTempLimit': false
+      }
+    ],
+    'metadata': {
+      'resultData': []
     }
   })
 }
