c01c36247c539c083117e9833f85be53519911de
Maciej Matys <maciej.matys@intive.com>
2017-08-22 16:49:48 +0200

    Stub correct login with card and pin

--- api/mocks/nedbank_money.js
+++ api/mocks/nedbank_money.js
@@ -1,20 +1,31 @@
 'use strict'
 
 module.exports = {
-  NedbankId_NedbankIdCredentials: loginByUserNameAndPassword
+  NedbankId_NedbankIdCredentials: loginByUserNameAndPassword,
+  NedbankId_NedbankIdCredentialsProfileCard: loginByCardAndPin
 }
 
 function loginByUserNameAndPassword (req, res) {
-  res.json({
-    'name': 'NedbankIdCredentials',
-    'response': {
-      'createAlias': false,
-      'authReference': '76be859d-2d23-45bf-8630-b5a9f7715585',
-      'authenticated': true
-    },
-    'result': {
-      'resultCode': 'string',
-      'resultMessage': 'string'
-    }
-  })
+  sendServiceResponse('NedbankIdCredentials', res)
+}
+
+function loginByCardAndPin (req, res) {
+  sendServiceResponse('NedbankIdCredentialsProfileCard', res)
+}
+
+function sendServiceResponse (serviceRespondName, res) {
+  serviceResponse.name = serviceRespondName
+  res.json(serviceResponse)
+}
+
+var serviceResponse = {
+  'response': {
+    'createAlias': false,
+    'authReference': '76be859d-2d23-45bf-8630-b5a9f7715585',
+    'authenticated': true
+  },
+  'result': {
+    'resultCode': 'string',
+    'resultMessage': 'string'
+  }
 }
