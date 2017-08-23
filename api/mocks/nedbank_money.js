ae4f34048c7649fcb152da8d4a84331e8e542f81
Maciej Matys <maciej.matys@intive.com>
2017-08-23 17:13:48 +0200

    Stub correct validation of username and password

--- api/mocks/nedbank_money.js
+++ api/mocks/nedbank_money.js
@@ -1,31 +1,29 @@
 'use strict'
 
 module.exports = {
-  NedbankId_NedbankIdCredentials: loginByUserNameAndPassword,
+  NedbankId_NedbankIdCredentials: validateNedbankIdUserNameAndPassword,
   NedbankId_NedbankIdCredentialsProfileCard: loginByCardAndPin
 }
 
-function loginByUserNameAndPassword (req, res) {
-  sendServiceResponse('NedbankIdCredentials', res)
+function validateNedbankIdUserNameAndPassword (req, res) {
+  response.createAlias = false
+  response.authReference = '76be859d-2d23-45bf-8630-b5a9f7715585'
+  response.authenticated = true
+  res.json(response)
 }
 
 function loginByCardAndPin (req, res) {
-  sendServiceResponse('NedbankIdCredentialsProfileCard', res)
+  sendResponse('NedbankIdCredentialsProfileCard', res)
 }
 
-function sendServiceResponse (serviceRespondName, res) {
-  serviceResponse.name = serviceRespondName
-  res.json(serviceResponse)
+function sendResponse (respondName, res) {
+  response.name = respondName
+  res.json(response)
 }
 
-var serviceResponse = {
-  'response': {
-    'createAlias': false,
-    'authReference': '76be859d-2d23-45bf-8630-b5a9f7715585',
-    'authenticated': true
-  },
+var response = {
   'result': {
-    'resultCode': 'string',
+    'resultCode': 0,
     'resultMessage': 'string'
   }
 }
