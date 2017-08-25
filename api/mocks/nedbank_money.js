40e18d9b6b542e58885959b59a23591fda396540
Maciej Matys <maciej.matys@intive.com>
2017-08-25 16:31:27 +0200

    Stub correct validation of pin profile password (PPP)

--- api/mocks/nedbank_money.js
+++ api/mocks/nedbank_money.js
@@ -2,18 +2,40 @@
 
 module.exports = {
   NedbankId_NedbankIdCredentials: validateNedbankIdUserNameAndPassword,
-  NedbankId_NedbankIdEnrolments: enrollByNedbankIdUserNameAndPassword,
+  NedbankId_NedbankIdEnrolments: enroll,
+  NedbankId_NedbankIdCredentialsProfile: validatePinProfilePassword,
   NedbankId_NedbankIdCredentialsProfileCard: loginByCardAndPin
 }
 
 function validateNedbankIdUserNameAndPassword (req, res) {
+  var response = new Response()
   response.createAlias = false
   response.authReference = '76be859d-2d23-45bf-8630-b5a9f7715585'
   response.authenticated = true
   res.json(response)
 }
 
-function enrollByNedbankIdUserNameAndPassword (req, res) {
+function enroll (req, res) {
+  res.json(new Response())
+}
+
+function validatePinProfilePassword (req, res) {
+  var response = new Response()
+  response.nedbankIdExist = true
+  response.oneTimePassword = '12345abc'
+  response.enterpriseCustomerNumber = 12345
+  response.tempId = 0
+  response.token = 'jas987adsuyhj2hjasd89sdjhdsaljkasd9sadjsadkj3'
+  response.identificationNumber = '1234567890123'
+  response.identificationType = 'RSAID'
+  response.countryCode = 'ZA'
+  response.dateOfBirth = '1950-01-01'
+  response.title = 'Mr'
+  response.emailAddress = 'test@test.com'
+  response.firstName = 'Stephan'
+  response.lastName = 'van Heerden'
+  response.authReference = '76be859d-2d23-45bf-8630-b5a9f7715585'
+  response.authenticated = true
   res.json(response)
 }
 
@@ -22,12 +44,12 @@ function loginByCardAndPin (req, res) {
 }
 
 function sendResponse (respondName, res) {
-  response.name = respondName
-  res.json(response)
+  Response.name = respondName
+  res.json(Response)
 }
 
-var response = {
-  'result': {
+function Response () {
+  this.result = {
     'resultCode': 0,
     'resultMessage': 'string'
   }
