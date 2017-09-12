cf4ef1f8d74da12e5b0ac3ef1da461d76491b635
pawanjashnani06 <pawan.jashnani@nagarro.com>
2017-09-12 12:58:12 +0530

    Update response for edit and delete contact card API. (#98)

--- api/mocks/contact_cards.js
+++ api/mocks/contact_cards.js
@@ -5,7 +5,8 @@ module.exports = {
   contactCardsDetailsPost: contactCardsDetailsPost,
   contactCardGet: contactCardGet,
   contactCardPut: contactCardPut,
-  contactCardGetLatest: contactCardGetLatest
+  contactCardGetLatest: contactCardGetLatest,
+  contactCardDelete: contactCardDelete
 }
 
 function contactCardsDetailsGet (req, res, next) {
@@ -3550,85 +3551,69 @@ function contactCardGet (req, res, next) {
 function contactCardPut (req, res, next) {
   res.json({
     'data': {
-      'contactCardID': 1001,
-      'contactCardName': 'Mark Twain',
+      'contactCardID': 77,
+      'contactCardName': 'INVEST',
       'contactCardDetails': [
         {
-          'beneficiaryID': 10101010,
-          'beneficiaryName': 'Mark Twain',
-          'accountNumber': '783421094571',
-          'bankName': 'Green Bank',
-          'branchCode': '110110',
-          'beneficiaryType': 'BDF',
-          'beneficiarySubType': 'INT',
-          'myReference': 'My debit references',
-          'beneficiaryReference': 'My creditor references'
+          'accountType': 'DS',
+          'beneficiaryID': 27,
+          'beneficiaryName': 'INVEST',
+          'accountNumber': '11951986-9993',
+          'bankName': 'NEDBANK',
+          'branchCode': '171338',
+          'beneficiaryType': 'BNFINT',
+          'myReference': 'INVEST_123555',
+          'beneficiaryReference': 'INVEST',
+          'valid': true
         }
       ],
       'contactCardNotifications': [
         {
-          'notificationAddress': '9886712834',
-          'notificationType': 'mobile',
-          'notificationDefault': true,
+          'notificationAddress': 'h@n.co.za',
+          'notificationType': 'EMAIL',
+          'notificationDefault': false,
           'notificationParents': [
             {
-              'beneficiaryID': 1,
-              'beneficiaryType': 'Sample text',
-              'beneficiarySubType': 'Sample text',
-              'notificationID': 1
+              'beneficiaryID': 27,
+              'beneficiaryType': 'BNFINT',
+              'notificationID': 5615520
+            }
+          ]
+        },
+        {
+          'notificationAddress': '',
+          'notificationType': 'FAX',
+          'notificationDefault': false,
+          'notificationParents': [
+            {
+              'beneficiaryID': 27,
+              'beneficiaryType': 'BNFINT',
+              'notificationID': 5615524
+            }
+          ]
+        },
+        {
+          'notificationAddress': '0619883752',
+          'notificationType': 'SMS',
+          'notificationDefault': false,
+          'notificationParents': [
+            {
+              'beneficiaryID': 27,
+              'beneficiaryType': 'BNFINT',
+              'notificationID': 5615234
             }
           ]
         }
       ]
     },
     'metadata': {
-      'page': 1,
-      'pageSize': 1,
-      'pageLimit': 1,
       'resultData': [
         {
-          'batchID': 'Sample text',
-          'transactionID': 'Sample text',
-          'recInstrID': 'Sample text',
           'resultDetail': [
             {
-              'operationReference': 'Sample text',
-              'result': 'Sample text',
-              'status': 'Sample text',
-              'reason': 'Sample text'
-            }
-          ],
-          'execEngineRef': 'Sample text',
-          'resultDetails': [
-            {
-              'operationReference': 'Sample text',
-              'result': 'Sample text',
-              'status': 'Sample text',
-              'reason': 'Sample text'
-            }
-          ]
-        }
-      ],
-      'resultSets': [
-        {
-          'batchID': 'Sample text',
-          'transactionID': 'Sample text',
-          'recInstrID': 'Sample text',
-          'resultDetail': [
-            {
-              'operationReference': 'Sample text',
-              'result': 'Sample text',
-              'status': 'Sample text',
-              'reason': 'Sample text'
-            }
-          ],
-          'execEngineRef': 'Sample text',
-          'resultDetails': [
-            {
-              'operationReference': 'Sample text',
-              'result': 'Sample text',
-              'status': 'Sample text',
-              'reason': 'Sample text'
+              'operationReference': 'BENEFICIARYSAVED',
+              'result': 'SUCCESS',
+              'status': 'SUCCESS'
             }
           ]
         }
@@ -3683,6 +3668,37 @@ function contactCardGetLatest (req, res, next) {
     },
     'metadata': {
       'resultData': []
+    }
+  })
+}
+
+function contactCardDelete (req, res, next) {
+  res.json({
+    'data': [
+      {
+        'contactCardID': 10,
+        'contactCardName': 'IGF 000655',
+        'contactCardDetails': [
+          {
+            'beneficiaryID': 6,
+            'beneficiaryType': 'BNFEXT'
+          }
+        ]
+      }
+    ],
+    'metadata': {
+      'resultData': [
+        {
+          'transactionID': '6',
+          'resultDetail': [
+            {
+              'operationReference': 'BENEFICIARYSAVED',
+              'result': 'R00',
+              'status': 'SUCCESS'
+            }
+          ]
+        }
+      ]
     }
   })
 }
