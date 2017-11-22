b2b028d461d45b40e357469ab583b6c4ebf207e0
Amitmittal01 <amit.mittal01@nagarro.com>
2017-11-22 13:20:18 +0530

    Add cards API (#128)

--- api/mocks/scp_clients.js
+++ api/mocks/scp_clients.js
@@ -5,7 +5,11 @@ module.exports = {
   updateClientDetails: updateClientDetails,
   updateClientsCards: updateClientsCards,
   getClientsCards: getClientsCards,
-  getClientPreferences: getClientPreferences
+  getClientPreferences: getClientPreferences,
+  clientCardDetails: clientCardDetails,
+  clientAccountCardsDetail: clientAccountCardsDetail,
+  blockCard: blockCard,
+  replaceCard: replaceCard
 }
 
 function clientDetails (req, res, next) {
@@ -23,32 +27,24 @@ function clientDetails (req, res, next) {
     'IdOrTaxIdNo': 6806016600086,
     'PassportNo': '24946300000000',
     'SecOfficerCd': '36300',
-    'AdditionalPhoneList': [
-      {
-        'AdditionalPhoneType': 'BUS',
-        'AdditionalPhoneNumber': '(011) 4724050'
-      },
-      {
-        'AdditionalPhoneType': 'CELL',
-        'AdditionalPhoneNumber': '27998016307'
-      },
-      {
-        'AdditionalPhoneType': 'HOME',
-        'AdditionalPhoneNumber': '(082) 8566440'
-      }
-    ],
+    'AdditionalPhoneList': [ {
+      'AdditionalPhoneType': 'BUS',
+      'AdditionalPhoneNumber': '(011) 4724050'
+    }, {
+      'AdditionalPhoneType': 'CELL',
+      'AdditionalPhoneNumber': '27998016307'
+    }, {
+      'AdditionalPhoneType': 'HOME',
+      'AdditionalPhoneNumber': '(082) 8566440'
+    } ],
     'Address': {
-      'AddressLines': [
-        {
-          'AddressLine': '3 EAGLE ROCK'
-        },
-        {
-          'AddressLine': 'EASTWOOD AVENUE'
-        },
-        {
-          'AddressLine': 'RANDPARK RIDGE'
-        }
-      ],
+      'AddressLines': [ {
+        'AddressLine': '3 EAGLE ROCK'
+      }, {
+        'AddressLine': 'EASTWOOD AVENUE'
+      }, {
+        'AddressLine': 'RANDPARK RIDGE'
+      } ],
       'AddressCity': 'JOHANNESBURG',
       'AddressPostalCode': '02169'
     }
@@ -63,14 +59,12 @@ function updateClientDetails (req, res, next) {
 }
 
 function getClientsCards (req, res, next) {
-  res.json([
-    {
-      'Template': 'Sample text',
-      'Title': 'Sample text',
-      'Heading': 'Sample text',
-      'CardData': {}
-    }
-  ])
+  res.json([ {
+    'Template': 'Sample text',
+    'Title': 'Sample text',
+    'Heading': 'Sample text',
+    'CardData': {}
+  } ])
 }
 
 function updateClientsCards (req, res, next) {
@@ -78,22 +72,135 @@ function updateClientsCards (req, res, next) {
 }
 
 function getClientPreferences (req, res, next) {
-  res.json([
-    {
-      'PreferenceKey': 'LayoutView',
-      'PreferenceValue': 'default'
+  res.json([ {
+    'PreferenceKey': 'LayoutView',
+    'PreferenceValue': 'default'
+  }, {
+    'PreferenceKey': 'PreferredName',
+    'PreferenceValue': 'Heineken'
+  }, {
+    'PreferenceKey': 'DefaultAccount',
+    'PreferenceValue': '21'
+  }, {
+    'PreferenceKey': 'FirstRunFinished',
+    'PreferenceValue': 'true'
+  } ])
+}
+
+function clientCardDetails (req, res, next) {
+  res.json({
+    'MetaData': {
+      'ResultCode': 'R00',
+      'Message': '',
+      'InvalidFieldList': null,
+      'result': {
+        'resultCode': 0,
+        'resultMessage': ''
+      }
     },
-    {
-      'PreferenceKey': 'PreferredName',
-      'PreferenceValue': 'Heineken'
+    'Data': [ {
+      'CamsPlasNbr': '558294 0000078511',
+      'CamsPlasStatus': 'Active',
+      'CamsPlasType': 'MJ3',
+      'DCIndicator': 'C',
+      'CardPlCustRelCd': 'ADI',
+      'CamsPlasStockCode': 'MJ3',
+      'PlCurStatRsnCd': 'AAA',
+      'PlasBranchNumber': '11',
+      'CamsNameLn': 'DR PAUL JOHN FOADEN',
+      'PlasExpiryDate': '2020-11-30 12:00:00 AM',
+      'PlasIssueDate': 'To be issued',
+      'PlasticTypeDescrip': 'MASTERCARD GOLD BUSINESS CREDIT',
+      'AllowBlock': true,
+      'AllowReplace': true,
+      'CardAccNumber': '589846 076124789 9',
+      'Owner': false,
+      'AvailBalance': 3250.0,
+      'AllowATMLimit': true,
+      'AllowBranch': true
+    }, {
+      'CamsPlasNbr': '558294 0000981219',
+      'CamsPlasStatus': 'Active',
+      'CamsPlasType': 'MJ3',
+      'DCIndicator': 'C',
+      'CardPlCustRelCd': 'ADI',
+      'CamsPlasStockCode': 'MJ3',
+      'PlCurStatRsnCd': '',
+      'PlasBranchNumber': '13',
+      'CamsNameLn': 'MASTER ROBIN SHUN HIP',
+      'PlasExpiryDate': '2020-11-30 12:00:00 AM',
+      'PlasIssueDate': '2017-11-07 12:00:00 AM',
+      'PlasticTypeDescrip': 'MASTERCARD GOLD BUSINESS CREDIT',
+      'AllowBlock': true,
+      'AllowReplace': false,
+      'CardAccNumber': '589846 076124789 9',
+      'Owner': false,
+      'AvailBalance': 3250.0,
+      'AllowATMLimit': true,
+      'AllowBranch': true
+    } ]
+  })
+}
+
+function clientAccountCardsDetail (req, res, next) {
+  res.json({
+    'MetaData': {
+      'ResultCode': 'R00',
+      'Message': '',
+      'InvalidFieldList': null,
+      'result': {
+        'resultCode': 0,
+        'resultMessage': ''
+      }
     },
-    {
-      'PreferenceKey': 'DefaultAccount',
-      'PreferenceValue': '21'
-    },
-    {
-      'PreferenceKey': 'FirstRunFinished',
-      'PreferenceValue': 'true'
+    'Data': [ {
+      'CamsPlasNbr': '558294 0000981219',
+      'CamsPlasStatus': 'Active',
+      'CamsPlasType': 'MJ3',
+      'DCIndicator': 'C',
+      'CardPlCustRelCd': 'ADI',
+      'CamsPlasStockCode': 'MJ3',
+      'PlCurStatRsnCd': '',
+      'PlasBranchNumber': '13',
+      'CamsNameLn': 'MASTER ROBIN SHUN HIP',
+      'PlasExpiryDate': '2020-11-30 12:00:00 AM',
+      'PlasIssueDate': '2017-11-07 12:00:00 AM',
+      'PlasticTypeDescrip': 'MASTERCARD GOLD BUSINESS CREDIT',
+      'AllowBlock': true,
+      'AllowReplace': false,
+      'CardAccNumber': '589846 076124789 9',
+      'Owner': false,
+      'AvailBalance': 3250.0,
+      'AllowATMLimit': true,
+      'AllowBranch': true
+    } ]
+  })
+}
+
+function blockCard (req, res, next) {
+  res.json({
+    'MetaData': {
+      'ResultCode': 'R00',
+      'Message': '',
+      'InvalidFieldList': null,
+      'result': {
+        'resultCode': 0,
+        'resultMessage': 'Block card request recieved'
+      }
     }
-  ])
+  })
+}
+
+function replaceCard (req, res, next) {
+  res.json({
+    'MetaData': {
+      'ResultCode': 'R00',
+      'Message': '',
+      'InvalidFieldList': null,
+      'result': {
+        'resultCode': 0,
+        'resultMessage': 'Replace card request recieved'
+      }
+    }
+  })
 }
